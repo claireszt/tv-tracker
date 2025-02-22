e41905131cf523ab3633dbc0512d7f65b15dae2b
Claire Szt <claireszt@gmail.com>
2025-02-22 03:43:39 +0100

    chore(commitlint): remove rules for subject-case

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,23 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feature",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "subject-case": [0],
+  },
 };
