91128ae25ba5efc70f6078d0f30d23e6cd13489f
Claire Szt <claireszt@gmail.com>
2025-03-02 19:38:34 +0100

    fix(eslint): upgrade lint with @next/eslint-plugin-next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
-import js from "@eslint/js";
+import pluginNext from "@next/eslint-plugin-next";
 import parser from "@typescript-eslint/parser";
 import prettier from "eslint-plugin-prettier";
 import react from "eslint-plugin-react";
 
 export default [
-  js.configs.recommended,
   {
+    name: "ESLint Config - nextjs",
     files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       parser,
@@ -15,12 +15,24 @@ export default [
         console: "readonly",
         URL: "readonly",
       },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
+      "@next/next": pluginNext,
       react,
       prettier,
     },
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     rules: {
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs["core-web-vitals"].rules,
+
       // ✅ Code Quality & Best Practices
       "no-unused-vars": "warn",
       "prefer-const": "warn",
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["artworks.thetvdb.com"],
+  },
 };
 
 export default nextConfig;
