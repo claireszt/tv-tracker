89e94c57c4d54aa0c640ecbd3f98f97f63b25283
Claire Szt <claireszt@gmail.com>
2025-03-12 12:19:01 +0100

    fix(design): fix build error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,5 @@
-"use client"; 
+"use client";
 
-import Button from "@/components/ui/Button";
 import Logo from "@/components/ui/Logo";
 
 export default function Dashboard() {
@@ -8,11 +7,11 @@ export default function Dashboard() {
     <div className="min-h-screen flex flex-col items-center justify-center gap-4 bg-light-background dark:bg-dark-background">
       <div className="flex flex-col items-center justify-center">
         <Logo />
-      <h1 className="mt-2 text-3xl font-bold text-light-text dark:text-dark-text font-heading">
-        Hello!
-      </h1>
-      <p>This page is under construction for now 🛠️</p>
-      <p>Come back later!</p>
+        <h1 className="mt-2 text-3xl font-bold text-light-text dark:text-dark-text font-heading">
+          Hello!
+        </h1>
+        <p>This page is under construction for now 🛠️</p>
+        <p>Come back later!</p>
       </div>
     </div>
   );
