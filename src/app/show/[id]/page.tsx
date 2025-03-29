d643a84884be5551342c58da70dfe963ddfc2e8f
Claire Szt <claireszt@gmail.com>
2025-03-29 02:26:14 +0100

    🐛 Fix checkbox icon display in watchlist buttons

--- src/app/show/[id]/page.tsx
+++ src/app/show/[id]/page.tsx
@@ -259,9 +259,33 @@ export default function ShowDetailPage() {
               icon={
                 showDetail.seasons
                   .flatMap((s) => s.episodes)
-                  .every((ep) => watchedEpisodes.includes(String(ep.id)))
-                  ? "checkbox"
-                  : "checkbox-checked"
+                  .every((ep) => watchedEpisodes.includes(String(ep.id))) ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M3 5a2 2 0 012-2h10a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5zm12 0H5v10h10V5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )
               }
             />
           )}
